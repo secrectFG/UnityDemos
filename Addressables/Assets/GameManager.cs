d303f96b28b8831fdc9aa40fecb8ec43e8ca568a
secrect <2724652+secrectFG@users.noreply.github.com>
2024-07-23 17:57:09 +0800

    更新释放逻辑

--- Addressables/Assets/GameManager.cs
+++ Addressables/Assets/GameManager.cs
@@ -1,25 +1,26 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
-
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 public class GameManager : MonoBehaviour
 {
     public string address = "Assets/UnityChan/SD_unitychan/Prefabs/ToonShader_SD_unitychan_humanoid_Illust.prefab";
 
-    // private IEnumerator Start() {
-    //     var op = Addressables.LoadAssetAsync<GameObject>(address);
-    //     yield return op;
-    //     Instantiate(op.Result);
-    // }
+    AsyncOperationHandle<GameObject> op;
 
     IEnumerator LoadCoroutine(string address) {
-        var op = Addressables.LoadAssetAsync<GameObject>(address);
+        op = Addressables.LoadAssetAsync<GameObject>(address);
         yield return op;
         Instantiate(op.Result);
     }
 
     public void Load(){
         StartCoroutine(LoadCoroutine(address));
+    }
+
+    public void UnLoad(){
+        //prefab引用的资源会被一并加载，所以用完的时候记得释放一下
+        Addressables.Release(op);
     }
 }
