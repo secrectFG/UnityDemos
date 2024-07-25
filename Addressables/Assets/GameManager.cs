408c269969beb9f33a8d0ba67dd0d45943d4505e
secrect <2724652+secrectFG@users.noreply.github.com>
2024-07-25 08:58:55 +0800

    更严谨的写法

--- Addressables/Assets/GameManager.cs
+++ Addressables/Assets/GameManager.cs
@@ -9,17 +9,23 @@ public class GameManager : MonoBehaviour
 
     AsyncOperationHandle<GameObject> op;
 
-    IEnumerator LoadCoroutine(string address) {
+    IEnumerator LoadCoroutine(string address)
+    {
         op = Addressables.LoadAssetAsync<GameObject>(address);
         yield return op;
-        Instantiate(op.Result);
+        if (op.Status == AsyncOperationStatus.Succeeded)
+            Instantiate(op.Result);
+        else 
+            Debug.LogError("Load Failed");
     }
 
-    public void Load(){
+    public void Load()
+    {
         StartCoroutine(LoadCoroutine(address));
     }
 
-    public void UnLoad(){
+    public void UnLoad()
+    {
         //prefab引用的资源会被一并加载，所以用完的时候记得释放一下
         Addressables.Release(op);
     }
